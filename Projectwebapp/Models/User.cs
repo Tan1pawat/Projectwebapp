faed9787a186574351fcb8da3cf65e63142cde6c
DrPawat <88647474+DrPawat@users.noreply.github.com>
2023-04-21 15:03:16 +0700

    v0.03

--- Projectwebapp/Models/User.cs
+++ Projectwebapp/Models/User.cs
@@ -1,8 +1,12 @@
-﻿namespace Projectwebapp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projectwebapp.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Pass { get; set; }
     }
